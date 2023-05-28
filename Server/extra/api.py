4f8e430f9a970e513cf4634ab8e60ba32e4b0f9d
Christian Gördes <christian.goerdes@outlook.de>
2023-05-28 17:49:16 +0200

    refactoring

--- Server/extra/api.py
+++ Server/extra/api.py
@@ -1,8 +1,8 @@
 try:
     import blackjack
 except ImportError as e:
-    print(f"Importing the shared library 'hangman' did not work.")
-    print(f"Is (a link to) the shared library 'hangman.____.so' in the same directory as this python script?")
+    print(f"Importing the shared library 'blackjack' did not work.")
+    print(f"Is (a link to) the shared library 'blackjack.____.so' in the same directory as this python script?")
     print(f"The import caused the following exception '{e}'")
     print(f"Exiting")
     exit(1)
