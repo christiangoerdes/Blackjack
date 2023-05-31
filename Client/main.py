9365f6035c2704b63bb88450cc6c789269dd75ea
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-05-31 22:38:58 +0200

    implemented asynchronous polling

--- Client/main.py
+++ Client/main.py
@@ -1,3 +1,4 @@
+import requests
 import asyncio
 from api import Api
 
@@ -5,13 +6,32 @@ global api_path
 PATH = "http://localhost:8000"
 
 
+async def poll_game_state(loop):
+    response = await loop.run_in_executor(None, requests.get, 'http://localhost:8000')
+
+    game_state = response.json
+
+    await asyncio.sleep(0.2)
+
+
+async def game_logic():
+    while True:
+        await asyncio.sleep(0.1)
+
+
+async def main():
+    loop = asyncio.get_event_loop()
+
+    tasks = [
+        loop.create_task(poll_game_state(loop)),
+        loop.create_task(game_logic())
+    ]
+
+    await asyncio.gather(*tasks)
+
 if __name__ == "__main__":
     global PATH
 
     api = Api()
-
-    # api._get_data(api_path)
-    # api._get_data(api_path + "/join?name=ibo&password=iboo")
-    # api._get_data(api_path)
 
     api.join(PATH, "my name", "my password")
